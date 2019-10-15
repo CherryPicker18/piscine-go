237eb6b9aef6976729e8f61020668dd50b024759
CherryPicker18 <namazbekov@gmail.com>
2019-10-15 18:55:57 +0600

    My 4th camp

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,3 @@
 curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID" '. [] | select(.id == $id) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
+
+
