43138a25ad432d09a2700decacf6df42cdb19b2d
CherryPicker18 <namazbekov@gmail.com>
2019-10-16 19:20:20 +0600

    isnegative

--- isnegative/isnegative.go
+++ isnegative/isnegative.go
@@ -1,11 +1,16 @@
-package main 
+package main
 
 import (
 	"github.com/01-edu/z01"
 )
 
-func IsNegative (nb int ){
-	if nb < 0 z01.PrintRune('F')
-	if nb >= 0 z01.PrintRune('T')
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('F')
+	}
+	if nb >= 0 {
+		z01.PrintRune('T')
+	}
+
+	z01.PrintRune('\n')
 }
-z01.printRune(\n)
