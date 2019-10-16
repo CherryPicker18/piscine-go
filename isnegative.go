cd56f68f34a6ed30407f6f39f0a1e6129143eb55
CherryPicker18 <namazbekov@gmail.com>
2019-10-16 20:49:37 +0600

    Test

--- isnegative.go
+++ isnegative.go
@@ -1,4 +1,4 @@
-package main
+package piscine
 
 import (
 	"github.com/01-edu/z01"
@@ -7,9 +7,10 @@ import (
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('F')
+		z01.PrintRune('\n')
 	} else {
 		z01.PrintRune('T')
+		z01.PrintRune('\n')
 	}
-
 	z01.PrintRune(10)
 }
