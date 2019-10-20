c8f1b6d4f9891212f31f8e137fa9f56e3fdc339e
CherryPicker18 <namazbekov@gmail.com>
2019-10-21 02:00:56 +0600

    Factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,18 +4,20 @@ import "github.com/01-edu/z01"
 
 func IterativeFactorial(nb int)int {
 
-	if nb <= 0 && nb >= 20{
-
+	if nb >= 0 && nb <= 20 {
+		
 	Fact:= 1
 	i:= 1
 	for ; i <= nb; i++ {
 
 		Fact *= i
 
+	}else {
+		return 0
 	} 
 
 		
 
 
 
-}
+} 
