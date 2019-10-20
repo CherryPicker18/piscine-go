f052caae5d5c73d15fa3c0e8bbcc1dbb653c360a
CherryPicker18 <namazbekov@gmail.com>
2019-10-21 01:54:59 +0600

    Factorial

---
+++ iterativefactorial.go
@@ -0,0 +1,21 @@
+package piscine 
+
+import "github.com/01-edu/z01"
+
+func IterativeFactorial(nb int)int {
+
+	if nb <= 0 && nb >= 20{
+
+	Fact:= 1
+	i:= 1
+	for ; i <= nb; i++ {
+
+		Fact *= i
+
+	} 
+
+	}
+
+
+
+}
