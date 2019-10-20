e7f10be6f9d6353f7057ae4f23cfda65ee2cc485
CherryPicker18 <namazbekov@gmail.com>
2019-10-21 02:52:58 +0600

    Factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-
 	if nb >= 0 && nb <= 20 {
-
 		Fact := 1
-
-		for i := 1; i <= nb; i++ {
-			Fact = Fact * i
-			return (Fact)
+		i := 1
+		for ; i <= nb; Fact *= i {
+			i++
 		}
+		return (Fact)
 	} else {
 		return 0
 	}
