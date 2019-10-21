93ebcc09381378383fb1c8526529b5c255321cdc
CherryPicker18 <namazbekov@gmail.com>
2019-10-21 17:34:52 +0600

    POwer_of_digit

--- iterativepower.go
+++ iterativepower.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 
-	result := nb // there we save digit
+	result := nb // there we save digit , result is digit =)
 	// 2 * 2 = 4 * 2 (digit = 2 ; if power = 3)
 	if power < 0 {
 		return 0
@@ -13,7 +13,7 @@ func IterativePower(nb int, power int) int {
 	}
 
 	for i := 2; i <= power; i++ { // begig from 2 cuz we don't need 0 and 1
-		digit = nb * digit // upper 2 if
+		result = nb * result // upper 2 if
 	}
 
 	return digit
