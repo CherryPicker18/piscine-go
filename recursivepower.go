cc20708bbf58dada36ae1d6bc53dc739c3149fa5
CherryPicker18 <namazbekov@gmail.com>
2019-10-21 18:50:16 +0600

    PowerByRecursive

--- recursivepower.go
+++ recursivepower.go
@@ -1,15 +1,11 @@
-package piscine 
+package piscine
 
 func RecursivePower(nb int, power int) int {
-
-
-	if power < 0 {
+	if power < 0 { // if power (-1) +/-; +/-
 		return 0
-	} 
-	if power == 0 {
+	}
+	if power == 0 { // anyway digit with power 0 = 1
 		return 1
 	}
-
-	return result nb * RecursivePower(nb, power - 1)
-
+	return nb * RecursivePower(nb, power-1) // RP(2,3)  2 * 4 = 8(2 * 2 * 2)
 }
