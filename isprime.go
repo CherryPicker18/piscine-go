e51c42f3d017e3e213e7480dfa8e48e358579025
CherryPicker18 <namazbekov@gmail.com>
2019-10-21 22:37:13 +0600

    IsPrime?

--- isprime.go
+++ isprime.go
@@ -6,20 +6,18 @@ func IsPrime(nb int) bool {
 	// если число = 0  - false
 	// если число = -1 - false
 
-	// нужен лог флажок ?
-	// isPrime := false
-
-	// if (nb%1 == 0) && (nb%nb == 0) {
-	// }
-
-
 	if nb == 1 || nb <= 0 {
 		return false
-	} else {
-		for i := 0; i < nb; i++ {
-			nb%i == 0
-			return true
-		}
 	}
 
+	isPrime := true
+
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 { // если делится на нацело - false
+			isPrime = false
+		}
+
+	}
+
+	return isPrime
 }
