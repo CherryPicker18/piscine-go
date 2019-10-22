3ec9c72d06e546ea1eaf6bc3509b1a25ef930835
CherryPicker18 <namazbekov@gmail.com>
2019-10-23 05:12:55 +0600

    printcomb_rework1

--- printcomb.go
+++ printcomb.go
@@ -1,25 +1,29 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
 func PrintComb() {
 
-	for a := '0'; a <= '9'; a++ {
-
-		for b := a + 1; b <= '9'; b++ {
-
-			for c := b + 1; c <= '9'; c++ {
-
-				z01.PrintRune(a)
-				z01.PrintRune(b)
-				z01.PrintRune(c)
-
-				if a < 55 {
+	for i := '0'; i <= '7'; i++ {
+		for j := '0'; j <= '8'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i < j && j < k && i < k {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
+
 			}
 		}
+
 	}
-	z01.PrintRune(10)
+
+}
+
+func main() {
+
+	PrintComb()
+
 }
