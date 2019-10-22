1cd748db2ffdb0b3bbb3254cdfd3db9ccf3fab82
CherryPicker18 <namazbekov@gmail.com>
2019-10-23 03:11:54 +0600

    trimatoi

---
+++ trimatoi.go
@@ -0,0 +1,34 @@
+package piscine 
+
+import "github.com/01-edu/z01"
+
+func TrimAtoi(s string) int{
+
+	runes := []rune(s) // Создан массив рун из строки
+	isFoundDigit := false // Создан логический флажок
+	result := 0 // иниц переменная для результата
+	sign := 0 // ициц переменная для символа
+	
+	for _, r := range runes {
+		if isFoundDigit == false {
+			// switch r { // я кароче гавнокодер сорри)
+			// case '-' sign = -1  не знаю как тут использовать оператор свитч)
+			// case '+' sign = 1  походу когда будет много else if подрят)
+			// }
+			if r == '-' {
+				sign = -1
+			} else if r == '+' {
+				sing = 1
+			}
+		}
+		
+		if r >= '0' && r <= '9'{
+			isFoundDigit = true 
+			result = 10 * result + int(r-'0')
+		}
+	}
+
+	return sign * result
+}
+
+
