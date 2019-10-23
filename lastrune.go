113cb3058dd17f1be669d193ef2f242888ab8959
CherryPicker18 <namazbekov@gmail.com>
2019-10-23 21:40:40 +0600

    lastrune

---
+++ lastrune.go
@@ -0,0 +1,21 @@
+package piscine
+
+func LastRune(s string) rune {
+
+	arror := []rune(s) // Создаётся слайс рун из строки
+
+	len := 0 // иниц перем для подсчета длины
+
+	for range arror {
+
+		len++
+	}
+
+	for index, value := range arror { // Пробежка по значениям массива
+		if index == len-1 {
+			return value
+		}
+	}
+
+	return 0 // суда нужна последняя руна
+}
