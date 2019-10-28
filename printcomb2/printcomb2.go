8fd2caf047dede04789ac87ffea53dedbae80f0f
CherryPicker18 <namazbekov@gmail.com>
2019-10-29 00:38:57 +0600

    Have many q-s

--- printcomb2/printcomb2.go
+++ printcomb2/printcomb2.go
@@ -4,26 +4,31 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 
-	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '9'; j++ {
-			for h := '0'; h <= '9'; h++ {
-				for k := '0'; k <= '9'; k++ {
-					if i < j && i > h || (i == h && j >= k) {
-						continue
-					}
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(32)
-					z01.PrintRune(h)
-					z01.PrintRune(k)
-					if !(i == '9' && j == '8' && h == '9' && k == '9') {
-						z01.PrintRune(44)
-						z01.PrintRune(32)
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			d := b + 1
+			for c := a; c <= '9'; c++ {
+				for ; d <= '9'; d++ {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(' ')
+					z01.PrintRune(c)
+					z01.PrintRune(d)
+
+					if a < '9' || b < '8' || c < '9' || d < '8' {
+
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 					}
 
 				}
+				d = '0'
 			}
+
 		}
+
 	}
+
 	z01.PrintRune(10)
+
 }
