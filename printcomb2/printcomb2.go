d031fcf0eb2c844a2ae6c06ae8de2e771ae5d781
CherryPicker18 <namazbekov@gmail.com>
2019-10-28 23:20:42 +0600

    reworked but have a lil q-s

--- printcomb2/printcomb2.go
+++ printcomb2/printcomb2.go
@@ -1,4 +1,4 @@
-package main
+package printcomb2
 
 import "github.com/01-edu/z01"
 
@@ -6,20 +6,17 @@ func PrintComb2() {
 
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
-			d := b + 1
+			z01.PrintRune(a)
+			z01.PrintRune(b)
+			z01.PrintRune(' ') // 32
 			for c := '0'; c <= '9'; c++ {
-				for ; d <= '9'; d++ {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(' ')
+				for d := '1'; d <= '9'; d++ {
 					z01.PrintRune(c)
 					z01.PrintRune(d)
-					if a != '9' || b != '8' || c != '9' || d != '9' {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
+					z01.PrintRune(',') // 44
+					z01.PrintRune(' ') // 32
+
 				}
-				d = '0'
 			}
 		}
 	}
